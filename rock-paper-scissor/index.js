56ba602c20feb19d8320d71c858a724bd0418d19
unknown <amanvermaspace@gmail.com>
2023-01-20 23:28:08 +0530

    created files for a new memory game and pushing somr changes for the rock paper scissor game

--- rock-paper-scissor/index.js
+++ rock-paper-scissor/index.js
@@ -3,18 +3,24 @@
 const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
 const result = document.getElementById('result');
+const compImage = document.querySelector('.computer-image');
+const userImage = document.querySelector('.user-image');
 
 const possibleChoices = document.querySelectorAll('.button');
 console.log(possibleChoices);
 
 let userChoice;     // initial value : undefined, scope : block
+let usrComparisonVar;
 let computerChoice; // initial value : undefined, scope : block
 
 for(let i = 0; i<possibleChoices.length; i++){
     // in this function, we are selecting either a rock, paper, or a scissor using a button
     possibleChoices[i].addEventListener('click', function(event){
-        userChoice = event.target.id;
-        userChoiceDisplay.innerHTML = userChoice;
+        userChoice = Number(event.target.id);
+        let arr = ["rock","paper","scissor"];
+        userChoiceDisplay.innerHTML = arr[userChoice-1];
+        usrComparisonVar = arr[userChoice-1];
+        userImage.src=`images/${userChoice}.png`
         generateComputerChoice();
         console.log(computerChoice);
         console.log(userChoice);
@@ -29,12 +35,15 @@ function generateComputerChoice() {
     switch(randomNumber) {
         case 1:
             computerChoice = "rock";
+            compImage.src = `images/${randomNumber}.png`
             break;
         case 2:
             computerChoice = "paper";
+            compImage.src = `images/${randomNumber}.png`
             break;
         case 3:
             computerChoice = "scissor";
+            compImage.src = `images/${randomNumber}.png`
             break;
     }
 
@@ -45,13 +54,13 @@ function checkResult() {
     // Add logic for when the computer wins
     // All the other cases will be when user wins
 
-    if(computerChoice === userChoice){
+    if(computerChoice === usrComparisonVar){
         result.innerHTML = "draw";
-    } else if(computerChoice === "scissor" && userChoice === "paper") {
+    } else if(computerChoice === "scissor" && usrComparisonVar === "paper") {
         result.innerHTML = "Computer Won";
-    } else if(computerChoice === "rock" && userChoice === "scissor") {
+    } else if(computerChoice === "rock" && usrComparisonVar === "scissor") {
         result.innerHTML = "Computer Won";
-    } else if(computerChoice === "paper" && userChoice === "rock") {
+    } else if(computerChoice === "paper" && usrComparisonVar === "rock") {
         result.innerHTML = "Computer Won";
     } else {
         result.innerHTML = "User won!"
